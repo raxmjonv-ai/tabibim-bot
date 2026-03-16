d2c0a5bd8c2be351776d7172cf5b140fbf650164
Muhammad Rahimjonov <muhammadrahimjonov@MacBook-Air-Muhammad.local>
2026-03-16 18:26:51 +0500

    update bot

--- bot.py
+++ bot.py
@@ -276,7 +276,7 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
         waiting_for_name.add(user_id)
 
         await update.message.reply_text(
-            "Ism va familiyangizni yozing:\n\nMasalan: Vali Karimov",
+            "Ism va familiyangizni yozing:\n\nMasalan: Malika Muslimova",
             reply_markup=ReplyKeyboardMarkup([["⬅️ Ortga"]], resize_keyboard=True)
         )
 
